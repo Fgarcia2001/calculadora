e345fc3ddce607562d9e00caf5119b917addc119
Franco Garcia <frangarciaoficial01@gmail.com>
2025-01-16 18:01:04 -0300

    en casa update

--- src/components/buttons/buttons.jsx
+++ src/components/buttons/buttons.jsx
@@ -1,8 +1,8 @@
-import style from "./buttons.module.css"
-export const Buttons = ({id, value, onClick}) =>{
-    return (
-        <div className={style.button} value={value} onClick={onClick}>
-            <p>{id}</p>
-        </div>
-    )
-}
+import style from "./buttons.module.css";
+export const Buttons = ({ id, value, onClick }) => {
+  return (
+    <button className={style.button} value={value} onClick={onClick}>
+      {id}
+    </button>
+  );
+};
--- src/components/display/display.jsx
+++ src/components/display/display.jsx
@@ -1,30 +1,57 @@
-import style from "./display.module.css"
-import { Buttons } from "../buttons/buttons.jsx"
-import { botones } from "./botones.js"
-import { useState } from "react"
+import style from "./display.module.css";
+import { Buttons } from "../buttons/buttons.jsx";
+import { botones } from "./botones.js";
+import { useState } from "react";
 export const Display = () => {
+  const [operacion, setOperacion] = useState("");
+  const handlerChange = (event) => {
+    setOperacion(event.target.value);
+  };
+  const handlerClick = (event) => {
+    setOperacion((prevOperacion) => prevOperacion + event.target.value);
+  };
 
-    const [operacion, setOperacion] = useState("");
-    const handlerChange = (event) =>{
-        setOperacion(event.target.value)
-    }
-    const handlerClick = (event) =>{
-        
-        setOperacion(prevOperacion => prevOperacion + event.target.value);  
-    }
-    return (
-        <div className={style.display}>
-            <div className={style.minipantalla}>
-                <input type="text" className={style.operacion} value={operacion} onChange={handlerChange}/>
-            </div>
-            <div className={style.botones}>
-                {botones.map((valor, index) => {
-                    return(
-                        <Buttons key={index} id={valor} value={valor} onClick={handlerClick}></Buttons>
-                    )
-                } )}
-            </div>
-        </div>
-    )
-}
+  const clear = () => {
+    setOperacion("");
+  };
 
+  const calcularResultado = () => {
+    try {
+      const operacionFormateada = operacion.replace(/x/gi, "*");
+      setOperacion(eval(operacionFormateada).toString());
+    } catch (error) {
+      setOperacion("Syntax Error");
+    }
+  };
+
+  return (
+    <div className={style.display}>
+      <div className={style.minipantalla}>
+        <input
+          type="text"
+          className={style.operacion}
+          value={operacion}
+          onChange={handlerChange}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") calcularResultado();
+          }}
+        />
+      </div>
+      <div className={style.botones}>
+        {botones.map((valor, index) => {
+          return (
+            <Buttons
+              key={index}
+              id={valor}
+              value={valor}
+              onClick={handlerClick}
+            ></Buttons>
+          );
+        })}
+      </div>
+      <div className={style.clear} onClick={clear}>
+        CLEAR
+      </div>
+    </div>
+  );
+};
