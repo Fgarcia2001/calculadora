2a8a7fd673c735d4b6e84fbf57488cf170b13bc7
Fgarcia2001 <frangarciaoficial01@gmail.com>
2025-01-16 14:36:06 -0300

    2do dia

--- src/components/display/display.jsx
+++ src/components/display/display.jsx
@@ -1,17 +1,26 @@
 import style from "./display.module.css"
 import { Buttons } from "../buttons/buttons.jsx"
 import { botones } from "./botones.js"
+import { useState } from "react"
 export const Display = () => {
 
+    const [operacion, setOperacion] = useState("");
+    const handlerChange = (event) =>{
+        setOperacion(event.target.value)
+    }
+    const handlerClick = (event) =>{
+        
+        setOperacion(prevOperacion => prevOperacion + event.target.value);  
+    }
     return (
         <div className={style.display}>
             <div className={style.minipantalla}>
-                pantalla
+                <input type="text" className={style.operacion} value={operacion} onChange={handlerChange}/>
             </div>
             <div className={style.botones}>
-                {botones.map((valor) => {
+                {botones.map((valor, index) => {
                     return(
-                        <Buttons id={valor}></Buttons>
+                        <Buttons key={index} id={valor} value={valor} onClick={handlerClick}></Buttons>
                     )
                 } )}
             </div>
